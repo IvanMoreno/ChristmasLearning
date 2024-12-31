6bd1b878d786695ca2346c3ad6d9a66811d834d0
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-12-31 19:47:36 +0100

    test: move boat backwards

--- Assets/ChristmasLearningProject/Tests/Editor/BoatTests.cs
+++ Assets/ChristmasLearningProject/Tests/Editor/BoatTests.cs
@@ -1,5 +1,6 @@
 using ChristmasLearningProject.Runtime.Domain;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using UnityEngine;
 
 public class BoatTests
@@ -14,5 +15,17 @@ public class BoatTests
         
         Assert.Greater(sut.Position.x, Vector2.zero.x);
         Assert.Greater(sut.Position.y, Vector2.zero.y);
+    }
+
+    [Test]
+    public void MoveBoatBackwards()
+    {
+        var sut = Boat.Cristal();
+        sut.SetRoute(Vector2.zero, Vector2.one * -1);
+        
+        sut.Move(1);
+        
+        Assert.Less(sut.Position.x, Vector2.zero.x);
+        Assert.Less(sut.Position.y, Vector2.zero.y);
     }
 }
