e3b3efc1692754e0e65be17bd3dd343c50ae5ae2
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-12-31 11:23:31 +0100

    refactor: move fleet to its own file

--- Assets/ChristmasLearningProject/Runtime/Domain/Boat.cs
+++ Assets/ChristmasLearningProject/Runtime/Domain/Boat.cs
@@ -1,24 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace ChristmasLearningProject.Runtime.Domain
 {
-    public class Fleet
-    {
-        readonly IList<Boat> members = new List<Boat>();
-        
-        public void Join(Boat member) => members.Add(member);
-
-        public void Move(float deltaTime)
-        {
-            foreach (var boat in members)
-            {
-                boat.Move(deltaTime);
-            }
-        }
-    }
-    
     public class Boat
     {
         public readonly string Category;
---
+++ Assets/ChristmasLearningProject/Runtime/Domain/Fleet.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace ChristmasLearningProject.Runtime.Domain
+{
+    public class Fleet
+    {
+        readonly IList<Boat> members = new List<Boat>();
+        
+        public void Join(Boat member) => members.Add(member);
+
+        public void Move(float deltaTime)
+        {
+            foreach (var boat in members)
+            {
+                boat.Move(deltaTime);
+            }
+        }
+    }
+}
