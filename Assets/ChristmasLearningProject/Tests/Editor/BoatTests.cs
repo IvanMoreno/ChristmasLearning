3e7dc53e06dc5c54cc4a80591bab800b55a35ff1
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-12-31 20:23:03 +0100

    test: reuse vector comparison api in assertions

--- Assets/ChristmasLearningProject/Tests/Editor/BoatTests.cs
+++ Assets/ChristmasLearningProject/Tests/Editor/BoatTests.cs
@@ -11,36 +11,34 @@ public class BoatTests
     {
         sut = Boat.Cristal();
     }
-    
+
     [Test]
     public void MoveBoatTowardsDestination()
     {
         sut.SetRoute(Vector2.zero, Vector2.one);
-        
+
         sut.Move(1);
-        
-        Assert.Greater(sut.Position.x, Vector2.zero.x);
-        Assert.Greater(sut.Position.y, Vector2.zero.y);
+
+        Assert.IsTrue(sut.Position.IsGreaterThan(Vector2.zero));
     }
 
     [Test]
     public void MoveBoatTowardsNegativeDirection()
     {
         sut.SetRoute(Vector2.zero, Vector2.one * -1);
-        
+
         sut.Move(1);
-        
-        Assert.Less(sut.Position.x, Vector2.zero.x);
-        Assert.Less(sut.Position.y, Vector2.zero.y);
+
+        Assert.IsTrue(sut.Position.IsLessThan(Vector2.zero));
     }
 
     [Test]
     public void StopAfterReachingDestination()
     {
         sut.SetRoute(Vector2.zero, Vector2.one);
-        
+
         sut.Move(10);
-        
+
         Assert.AreEqual(Vector2.one, sut.Position);
     }
 
@@ -48,12 +46,12 @@ public class BoatTests
     public void MoveEvenly()
     {
         sut.SetRoute(Vector2.zero, Vector2.one);
-        
+
         sut.Move(0.01f);
         var firstStepDistance = sut.Position;
         sut.Move(0.01f);
         var secondStepDistance = sut.Position;
-        
+
         Assert.AreEqual(firstStepDistance - Vector2.zero, secondStepDistance - firstStepDistance);
     }
 
@@ -61,10 +59,10 @@ public class BoatTests
     public void RewindMovement()
     {
         sut.SetRoute(Vector2.zero, Vector2.one);
-        
+
         sut.Move(0.01f);
         sut.Rewind(0.01f);
-        
+
         Assert.AreEqual(Vector2.zero, sut.Position);
     }
 
@@ -72,31 +70,29 @@ public class BoatTests
     public void Position_IsNotClamped_DuringRewind()
     {
         sut.SetRoute(Vector2.zero, Vector2.one);
-        
+
         sut.Rewind(1);
-        
-        Assert.Less(sut.Position.x, Vector2.zero.x);
-        Assert.Less(sut.Position.y, Vector2.zero.y);
+
+        Assert.IsTrue(sut.Position.IsLessThan(Vector2.zero));
     }
 
     [Test]
     public void PositionClamping_TakesIntoAccount_MovementDirection()
     {
         sut.SetRoute(Vector2.one, Vector2.zero);
-        
+
         sut.Move(0.01f);
-        
-        Assert.Greater(sut.Position.x, Vector2.zero.x);
-        Assert.Greater(sut.Position.y, Vector2.zero.y);
+
+        Assert.IsTrue(sut.Position.IsGreaterThan(Vector2.zero));
     }
 
     [Test]
     public void ClampPosition_AfterReaching_Destination_InNegativeDirection()
     {
         sut.SetRoute(Vector2.one, Vector2.zero);
-        
+
         sut.Move(10);
-        
+
         Assert.AreEqual(Vector2.zero, sut.Position);
     }
 }
