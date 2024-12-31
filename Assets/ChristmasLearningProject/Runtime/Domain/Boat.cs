7363feafbef29a2867e9a04ec51d09ddc3e436ef
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-12-31 20:29:45 +0100

    refactor: introduce route notion

--- Assets/ChristmasLearningProject/Runtime/Domain/Boat.cs
+++ Assets/ChristmasLearningProject/Runtime/Domain/Boat.cs
@@ -7,36 +7,30 @@ namespace ChristmasLearningProject.Runtime.Domain
     {
         const int Speed = 10;
         public Vector2 Position { get; private set; }
-        
-        Vector2 departure;
-        Vector2 destination;
+        Route route;
 
         public void Rewind(float deltaTime) => Move(-deltaTime);
 
         public void Move(float deltaTime)
         {
-            if (departure.Equals(destination))
-                throw new InvalidOperationException("A valid route must be specified first");
-
-            Position += (destination - departure).normalized * (deltaTime * Speed);
+            Position += route.Direction * (deltaTime * Speed);
             ClampPosition();
         }
 
         void ClampPosition()
         {
-            if (destination.IsGreaterThan(departure) && Position.IsGreaterThan(destination) || 
-                departure.IsGreaterThan(destination) && Position.IsLessThan(destination))
-                Position = destination;
+            if (!route.ReachedDestinationAt(Position)) return;
+            
+            Position = route.Destination;
         }
 
         public void SetRoute(Vector2 departure, Vector2 destination)
         {
             if (departure.Equals(destination))
                 throw new ArgumentException("departure and destination must differ");
-            
-            this.departure = departure;
-            this.destination = destination;
-            Position = this.departure;
+
+            route = new Route(departure, destination);
+            Position = route.Departure;
         }
 
         public static Boat Cristal() => new();
---
+++ Assets/ChristmasLearningProject/Runtime/Domain/Route.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace ChristmasLearningProject.Runtime.Domain
+{
+    public readonly struct Route
+    {
+        public readonly Vector2 Departure;
+        public readonly Vector2 Destination;
+        public Vector2 Direction => (Destination - Departure).normalized;
+
+        public Route(Vector2 departure, Vector2 destination)
+        {
+            if (departure.Equals(destination))
+                throw new InvalidOperationException("A valid route must be specified first");
+            
+            Departure = departure;
+            Destination = destination;
+        }
+
+        public bool ReachedDestinationAt(Vector2 position)
+        {
+            return Destination.IsGreaterThan(Departure) && position.IsGreaterThan(Destination) ||
+                   Departure.IsGreaterThan(Destination) && position.IsLessThan(Destination);
+        }
+    }
+}
