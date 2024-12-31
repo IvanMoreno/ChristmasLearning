e9f55285734d4c7fbbcda0606939c5af20edc318
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2024-12-31 19:48:13 +0100

    refactor: remove unnecessary argument

--- Assets/ChristmasLearningProject/Runtime/Domain/Boat.cs
+++ Assets/ChristmasLearningProject/Runtime/Domain/Boat.cs
@@ -5,13 +5,11 @@ namespace ChristmasLearningProject.Runtime.Domain
 {
     public class Boat
     {
-        public readonly string Category;
         public Vector2 Position { get; private set; }
         
         Vector2 departure;
         Vector2 destination;
-        Boat(string category) => Category = category;
-
+        
         public void Move(float deltaTime)
         {
             Position += (destination - Position) * (deltaTime * 10);
@@ -24,7 +22,7 @@ namespace ChristmasLearningProject.Runtime.Domain
             Position = this.departure;
         }
 
-        public static Boat Cristal() => new("Cristal");
-        public static Boat Shield() => new("Shield");
+        public static Boat Cristal() => new();
+        public static Boat Shield() => new();
     }
 }
