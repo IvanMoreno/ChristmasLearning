d3981aa5032eb902a7bfede9c14f60f4f5a37ddb
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-01-01 17:22:17 +0100

    test: increase test timescale

--- Assets/ChristmasLearningProject/Tests/Runtime/GameplayTests.cs
+++ Assets/ChristmasLearningProject/Tests/Runtime/GameplayTests.cs
@@ -19,7 +19,14 @@ namespace ChristmasLearningProject.Tests.Runtime
         [UnitySetUp]
         public IEnumerator SetUp()
         {
+            Time.timeScale = 5;
             yield return FromLevelEditor().WithShieldBoatEnabled().Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Time.timeScale = 1;
         }
         
         [Test]
