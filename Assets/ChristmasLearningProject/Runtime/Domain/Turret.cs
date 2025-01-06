c15a4a7c90824e3898020b42b84e4633af995a67
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-01-06 13:34:16 +0100

    refactor: split is in range method

--- Assets/ChristmasLearningProject/Runtime/Domain/Turret.cs
+++ Assets/ChristmasLearningProject/Runtime/Domain/Turret.cs
@@ -7,7 +7,8 @@ namespace ChristmasLearningProject.Runtime.Domain
     public class Turret
     {
         public const float MaxDetectionDistance = 5;
-        
+        public const float MaxVisionAngle = 120;
+
         readonly Vector2 position;
         readonly Vector2 facingDirection;
 
@@ -17,16 +18,14 @@ namespace ChristmasLearningProject.Runtime.Domain
             this.facingDirection = facingDirection;
         }
 
-        public void Attack(Fleet invaders)
-        {
-            NearestInvader(invaders)?.ReceiveDamage();
-        }
+        public void Attack(Fleet invaders) => NearestInvader(invaders)?.ReceiveDamage();
 
         Boat NearestInvader(Fleet invaders) => invaders.Members.OrderBy(DistanceToMe).FirstOrDefault(IsInRange);
 
         float DistanceToMe(Boat invader) => Distance(invader.Position, position);
-        bool IsInRange(Boat invader) => Distance(invader.Position, position) <= MaxDetectionDistance && Angle(facingDirection,
-            invader.Position - position) <= 120;
+        bool IsInRange(Boat invader) => IsNearEnough(invader) && IsInsideVisionRange(invader);
+        bool IsNearEnough(Boat invader) => Distance(invader.Position, position) <= MaxDetectionDistance;
+        bool IsInsideVisionRange(Boat invader) => Angle(facingDirection, invader.Position - position) <= MaxVisionAngle;
 
         public static Turret Ensemble(Vector2 position, Vector2 facingDirection) => new(position, facingDirection);
     }
