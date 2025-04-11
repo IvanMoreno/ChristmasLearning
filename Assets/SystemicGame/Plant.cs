c6c70cd237686ca71e89c0973508b176b9261367
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-04-11 17:40:27 +0200

    propagate fire

--- Assets/SystemicGame/Plant.cs
+++ Assets/SystemicGame/Plant.cs
@@ -8,8 +8,17 @@ public class Plant : Entity
         if (stimuli != "Fire") return;
         
         GetComponent<MeshRenderer>().material = burnedMaterial;
-        
+
         if (gameObject.GetComponent<Flame>() == null)
+        {
             gameObject.AddComponent<Flame>();
+            var nearby = Physics.OverlapSphere(transform.position, 1);
+            foreach (var nearbyEntity in nearby)
+            {
+                if (!nearbyEntity.TryGetComponent(typeof(Entity), out var entity)) return;
+
+                entity.SendMessage("Perceive", "Fire");
+            }
+        }
     }
 }
