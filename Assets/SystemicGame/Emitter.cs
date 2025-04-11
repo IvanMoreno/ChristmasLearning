ad9a0d059cd98cf1443786c705da90fd298fd64a
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-04-11 17:52:41 +0200

    fafsadfas

--- Assets/SystemicGame/Emitter.cs
+++ Assets/SystemicGame/Emitter.cs
@@ -9,16 +9,12 @@ public class Emitter : MonoBehaviour
         var nearby = Physics.OverlapSphere(transform.position, 1);
         foreach (var nearbyEntity in nearby)
         {
-            if (!nearbyEntity.TryGetComponent(typeof(Emitter), out var entity)) return;
-
-            entity.SendMessage("Perceive", signal);
+            nearbyEntity.SendMessage("Perceive", signal);
         }
     }
     
     void OnTriggerEnter(Collider other)
     {
-        if (!other.TryGetComponent(typeof(Emitter), out var entity)) return;
-
-        entity.SendMessage("Perceive", signal);
+        other.SendMessage("Perceive", signal);
     }
 }
