4dfd5899295a214a76cce9ab09fe57ceadb659fa
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-04-11 18:02:49 +0200

    dsfaf

--- Assets/SystemicGame/Plant.cs
+++ Assets/SystemicGame/Plant.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 
 public class Plant : MonoBehaviour
 {
     public Material burnedMaterial;
+    public Material plantMaterial;
 
     public void Perceive(string stimuli)
     {
@@ -14,6 +16,14 @@ public class Plant : MonoBehaviour
         if (gameObject.GetComponent<Emitter>() == null)
         {
             gameObject.AddComponent<Emitter>().signal = reactiveTo;
+        }
+    }
+
+    void Update()
+    {
+        if (gameObject.GetComponent<Emitter>() == null)
+        {
+            GetComponent<MeshRenderer>().material = plantMaterial;
         }
     }
 }
