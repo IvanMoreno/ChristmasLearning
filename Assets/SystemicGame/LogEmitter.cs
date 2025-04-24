d31b4908162fb2c53ce96865bcffc08619e50d6d
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-04-24 19:13:46 +0200

    refactor: move files

--- Assets/SystemicGame/LogEmitter.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LogEmitter : MonoBehaviour
-{
-    public void Perceive(string stimuli)
-    {
-        Debug.Log("He percibido el estimulo " + stimuli);
-    }
-}
